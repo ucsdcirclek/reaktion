adcca67c7ee63c4bbc7600dd84fda776a466dd0c
Jerome Lam <j2lam@ucsd.edu>
2020-09-17 17:41:32 -0700

    Ditched Docker and Added Firebase
    Leaving docker for now because can't figure out how to connect to firebase; and connected firebase to react can be found on the drive

--- homestead/src/index.js
+++ homestead/src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCteNUgd09tUWNYeaPEn7meNR-SzdwmgQ0",
+    authDomain: "reaktion-4f443.firebaseapp.com",
+    databaseURL: "https://reaktion-4f443.firebaseio.com",
+    projectId: "reaktion-4f443",
+    storageBucket: "reaktion-4f443.appspot.com",
+    messagingSenderId: "288213449957",
+    appId: "1:288213449957:web:16aca123e2fd1fde63280c",
+    measurementId: "G-B25G1MPX53"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
 
 ReactDOM.render(
   <React.StrictMode>
