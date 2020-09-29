9c83ccd64c7b1d950940ee7ce9a6b30edcdab9e7
vkelkar1999 <vkelkar@ucsd.edu>
2020-09-28 17:09:00 -0700

    Facebook SDK

--- homestead/src/App.js
+++ homestead/src/App.js
@@ -6,6 +6,30 @@ import Profile from './Profile';
 import Leaderboard from './Leaderboard';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{your-app-id}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+
 function App() {
   return (
     <Router>
