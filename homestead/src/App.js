11ccfdad86154ced80bfffbc2f0f1400d0856b5a
Jerome Lam <55860345+J3r0m3L@users.noreply.github.com>
2020-12-03 10:12:49 -0800

    New Fam Pages!

--- homestead/src/App.js
+++ homestead/src/App.js
@@ -6,6 +6,10 @@ import About from './about/About.js';
 import Teams from './teams/Teams.js';
 import AboutMembership from './about/AboutMembership.js';
 import AboutCommunity from './about/AboutCommunity.js';
+import AboutClouds from './about/families/AboutClouds.js';
+import AboutFlowers from './about/families/AboutFlowers.js';
+import AboutLotus from './about/families/AboutLotus.js';
+import AboutSun from './about/families/AboutSun.js';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -21,10 +25,10 @@ function App() {
           {/*Calendar*/}
           {/*Teams*/}
           {/*Admin*/}
-          <Route path="/about/community/whitelotus"></Route>
-          <Route path="/about/community/farmersoverflowers"></Route>
-          <Route path="/about/community/breathofthesun"></Route>
-          <Route path="/about/community/headintheclouds"></Route>
+          <Route path="/about/community/whitelotus"><AboutLotus/></Route>
+          <Route path="/about/community/farmersoverflowers"><AboutFlowers/></Route>
+          <Route path="/about/community/breathofthesun"><AboutSun/></Route>
+          <Route path="/about/community/headintheclouds"><AboutClouds/></Route>
           <Route path="/about/membership"><AboutMembership /></Route>
           <Route path="/about/community"><AboutCommunity /></Route>
           <Route path="/about"><About /></Route>
---
+++ homestead/src/about/families/AboutLotus.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AboutLotus.css';
+
+function AboutLotus() {
+  return (
+    <div className='aboutLotus'>
+      <h1>Lotus</h1>
+    </div>
+  );
+}
+
+export default AboutLotus;
