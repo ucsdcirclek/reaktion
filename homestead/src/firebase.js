7b18bc233459b1c6f06e52acd7ce41cbfecf6103
jerome2l <55860345+J3r0m3L@users.noreply.github.com>
2021-04-21 13:59:19 -0700

    Added env files

--- homestead/src/firebase.js
+++ homestead/src/firebase.js
@@ -3,14 +3,14 @@ import firebase from "firebase";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCteNUgd09tUWNYeaPEn7meNR-SzdwmgQ0",
-  authDomain: "reaktion-4f443.firebaseapp.com",
-  databaseURL: "https://reaktion-4f443.firebaseio.com",
-  projectId: "reaktion-4f443",
-  storageBucket: "reaktion-4f443.appspot.com",
-  messagingSenderId: "288213449957",
-  appId: "1:288213449957:web:16aca123e2fd1fde63280c",
-  measurementId: "G-B25G1MPX53"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
