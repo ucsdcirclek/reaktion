912477aad0baae99d1ae5180803d5008f3622a84
Navina Mahesh <navinamahesh@gmail.com>
2021-05-03 19:49:06 -0700

    updated the footer

--- homestead/src/Footer.js
+++ homestead/src/Footer.js
@@ -9,16 +9,23 @@ import CopyrightIcon from "@material-ui/icons/Copyright";
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer_socialMedia">
-        <a href="https://www.facebook.com/groups/ucsdcki"><FacebookIcon /></a>
-        <a href="https://instagram.com/ucsdcirclek"><TwitterIcon /></a>
-        <a href="https://twitter.com/ucsdcircle"><InstagramIcon /></a>
-      </div>
-      <div className="footer_copyRight">
-        <CopyrightIcon />
-        <p>2017-2020 UC San Diego Circle K International</p>
-      </div>
-      <img src={masthead} className="footer_image"/>
+        <img class="footer_image" src="/src/images/footer_left.png">
+
+        <div class="footer_text">
+          <div className="footer_socialMedia">
+            <a href="https://www.facebook.com/groups/ucsdcki"><FacebookIcon /></a>
+            <a href="https://instagram.com/ucsdcirclek"><TwitterIcon /></a>
+            <a href="https://twitter.com/ucsdcircle"><InstagramIcon /></a>
+          </div>
+
+          <div className="footer_copyRight">
+            <CopyrightIcon />
+            <p>2017-2020 UC San Diego Circle K International</p>
+          </div>
+         </div>
+
+        <img class="footer_image footer_image_left" src="/src/images/footer_right.png">
+
     </div>
   );
 }
