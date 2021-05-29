1ca7a86acb2460cabe3b78e4bab9c3a240c99b79
jerome2l <55860345+J3r0m3L@users.noreply.github.com>
2021-05-28 19:58:38 -0700

    More css changes

--- homestead/src/about/subCategories/OurClub.js
+++ homestead/src/about/subCategories/OurClub.js
@@ -1,16 +1,110 @@
 import React from "react";
 import Navbar from "./AboutNavbar.js";
+import Board from "../Board.js";
 import Footer from "../../Footer.js";
 import "./OurClub.css";
+import Amber from "../../images/board/Amber.jpg";
+import An from "../../images/board/An.jpg";
+import Anna from "../../images/board/Anna.png";
+import EmilyC from "../../images/board/EmilyC.jpg";
+import EmilyKw from "../../images/board/EmilyKw.jpg";
+import Helen from "../../images/board/Helen.jpg";
+import Jerome from "../../images/board/Jerome.jpg";
+import Joaquin from "../../images/board/Joaquin.jpg";
+import Marlenne from "../../images/board/Marlenne.jpg";
+import Sarah from "../../images/board/Sarah.jpg";
+import Shannon from "../../images/board/Shannon.jpg";
+import Sophia from "../../images/board/Sophia.jpg";
 
 function OurClub() {
   return (
     <div className="ourClub">
       <Navbar/>
       <div className="ourClub_sub">
-
+        <div className="ourClub_text1">
+          <h2>Overview</h2>
+          <p>First chartered in 1977, Circle K International at UC San Diego is the largest community service organization on campus. 
+          UCSD CKI distinguishes itself from other campus organizations for its dedications to <b>service</b>, <b>leadership</b>, and <b>fellowship</b>. We 
+          participate in service projects including: La Jolla Kiwanis Club, Boys & Girls Club, Ladle Soup Kitchen, San Diego Food Bank, 
+          American Red Cross, Reality Changers, and more. Our doors are always open to new members and we encourage you to come and get 
+          involved today!</p>
+        </div>
+        <div className="ourClub_text1">
+          <h2>General Body Meetings</h2>
+          <p>UCSD Circle K holds general body meetings generally every week to discuss upcoming events, recognize members, and just have 
+          some all around fun. Meetings are open to everyone and details can be found on the website calendar.</p>
+        </div>
+        <div className="ourClub_events">
+          <h2>Key Events</h2>
+          <div>
+            <div className="ourClub_rows">
+              <p>District Large Scale Service Project (DLSSP)</p>
+              <p>Fall Training Conference (FTC)</p>
+            </div>
+            <div className="ourClub_rows">
+              <p>Crazy Kompetition for Infants (CKI South)</p>
+              <p>District Convention (DCON)</p>
+            </div>
+            <div className="ourClub_rows">
+              <p>Masquerade Ball</p>
+              <p>New Member Installation</p>
+            </div>
+          </div>
+        </div>
+        <div className="ourClub_events">
+          <h2>Past Club Themes</h2>
+          <ul>
+            <li>2020-2021: Garden</li>
+            <li>2019-2020: Biomes</li>
+            <li>2018-2019: Space</li>
+            <li>2017-2018: Blast to the Past</li>
+            <li>2016-2017: Adventure</li>
+          </ul>
+          <ul>
+            <li>2015-2016: Under the Sea</li>
+            <li>2014-2015: Fairy Tales</li>
+            <li>2012-2013: Spies</li>
+            <li>2011-2012: The Nightmare Before Christmas</li>
+          </ul>
+        </div>
+        <div className="ourClub_text1">
+          <h2>Kiwanis</h2>
+          <div>
+            <p>Kiwanis International is a global community of clubs, members, and partners dedicated to improving the lives of children 
+            one community at a time. Today, Kiwanis International has over 550,000 members from K-Kids to Key Club to Kiwanis stretching 
+            between 80 countries and hosting around 150,000 service projects per year. UCSD Circle K is closely associated with the Kiwanis 
+            Club of La Jolla which is proud to serve the needs of La Jolla Village and its surrounding neighborhood communities.</p>
+            <h3>Kiwanis International website can be found <a href="https://www.kiwanis.org/"><b>here</b></a></h3>
+            <h3>Kiwanis Club of La Jolla can be found <a href="https://kiwanisclublajolla.org/"><b>here</b></a></h3>
+          </div>
+        </div>
       </div>
-      <Footer/>
+      <div className='ourClub_board'>
+          <h2>2020-2021 UCSD CKI Board</h2>
+          <h3>Executive Board</h3>
+          <div className='ourClub_execBoard'>
+            <Board image={An} name='An Nguyen' title='Vice President of Administration' email='vpa@ucsdcki.org'/>
+            <Board image={Helen} name='Helen Huynh' title='Vice President of Service' email='vps@ucsdcki.org'/>
+            <Board image={Shannon} name='Shannon Lo' title='President' email='president@ucsdcki.org'/>
+            <Board image={Joaquin} name='Joaquin Ramento' title='Secretary' email='secretary@ucsdcki.org'/>
+            <Board image={EmilyC} name='Emily Chiew' title='Treasurer' email='treasurer@ucsdcki.org'/>
+          </div>
+          <h3>Appointed Board</h3>
+          <div className='ourClub_appointedBoard1'>
+            <Board image={Sophia} name='Sophia Jordan' title='Kiwanis Family Chair' email='kfam@ucsdcki.org'/>
+            <Board image={Sarah} name='Sarah Fan' title='Public Relations' email='pr@ucsdcki.org'/>
+            <Board image={Amber} name='Amber Pan' title='Member Relations' email='mr@ucsdcki.org'/>
+            <Board image={EmilyKw} name='Emily Kwong' title='Social' email='social@ucsdcki.org'/>
+          </div>
+          <div className='ourClub_appointedBoard2'>
+            <Board image={Anna} name='Anna Zhou' title='Fellowship' email='fellowship@ucsdcki.org'/>
+            <Board image={Marlenne} name='Marlenne Cota' title='Service' email='service@ucsdcki.org'/>
+            <Board image={Jerome} name='Jerome Lam' title='Technology' email='technology@ucsdcki.org'/>
+          </div>
+        </div>
+      <div className='ourClub_footer'>
+        <Footer/>
+      </div>
     </div>
   );
 }
--- homestead/src/about/subCategories/Structure.js
+++ homestead/src/about/subCategories/Structure.js
@@ -8,8 +8,86 @@ function Structure() {
     <div className="structure">
       <Navbar />
         <div className="structure_sub">
-
-          <h3>This is Structure</h3>
+          <div className='structure_text1'>
+            <h2>Scope</h2>
+            <p>Circle K International operates on an international, sub-regional, district, divisional, and club level, 
+            and is comprised of over 13,000 members who make up over 500 clubs in 30 districts and 17 nations, including 
+            Antigua, Australia, Bahamas, Barbados, Canada, Cayman Islands, Colombia, Dominican Republic, Ecuador, 
+            Jamaica, Malaysia, México, Nigeria, Panama, Philippines, St. Lucia, Suriname, Taiwan and the United 
+            States.</p>
+          </div>
+          <div className='structure_text1'>
+            <h2>Club</h2>
+            <p>This level is comprised of members within a particular school or university. Here, a board of officers, 
+            executive board and appointed Board runs the operations of the club.Executive Board positions include: President, 
+            Administrative Vice President, Service Vice President, Treasurer, and Secretary. Appointed Board positions 
+            include: Social Chair, Fundraising Chair, Kiwanis Family Chair, Public Relations Chair, Member Relations Chair, 
+            Member Development and Education Chair, Fellowship Chair, Service Chair, and Technology Chair.</p>
+          </div>
+          <div className='structure_text1'>
+            <h2>Division</h2>
+            <div>
+              <p>Each CKI division is composed of clubs within a close demographic area. UCSD Circle K is part of Paradise Division, 
+              one of the eight divisions of Cal-Nev-Ha and is made up of seven schools ranging from California to Hawaii. Our division’s 
+              mascots are the <b>toucan</b> and a <b>pineapple.</b></p>
+              <h3>Paradise division's website can be found <a href="http://www.cnhcirclek.org/divisions/paradise/"><b>here</b></a></h3>
+            </div>
+          </div>
+          <div className='structure_clubs'>
+            <h2>Paradise Clubs</h2>
+            <div className='structure_list'>
+              <div className='structure_row1'>
+                <h2><a href="https://www.sdsucirclek.org/">San Diego State University</a></h2>
+                <h2>University of California, San Diego</h2>
+                <h2><a href="https://csusmcki.wixsite.com/csusmcki">California State University, San Marcos</a></h2>
+              </div>
+              <div className='structure_row2'>
+                <h2>University of Hawaii, Hilo</h2>
+                <h2><a href='http://www2.hawaii.edu/~cki/#intro'>University of Hawaii, Manoa</a></h2>
+                <h2>Hawaii Pacific University</h2>
+              </div>
+              <div className='structure_row3'>
+                <h2><a href='https://csusmcki.wixsite.com/csusmcki/circle-k-info'>Palomar College</a></h2>
+                <h2><a href='https://gcsummit.com/11637/uncategorized/the-circle-k-of-grossmont/'>Grossmont College</a></h2>
+              </div>
+            </div>
+          </div>
+          <div className='structure_text1'>
+            <h2>District</h2>
+            <div>
+              <p>Each division then falls under a district which is usually bounded by state lines. During the annual district convention, 
+              delegates from all clubs within a district elect a Governor Secretary and Treasurer to serve on the District Executive Board. 
+              A board of district chairs are then appointed to carry out specific district focuses and events throughout the year in which 
+              clubs from all over the district can participate and attend. UCSD Circle K is part of the largest district in CKI, the Cal-Nev-Ha 
+              district, which comprises California, Nevada, and Hawaii. Our district spans over 2700 miles and holds 40 clubs with over hundreds 
+              of members.</p>
+              <h3>CNH Circle K's website can be found <a href="http://www.cnhcirclek.org/"><b>here</b></a></h3>
+              <h3>CNH district resources can be found <a href="http://resources.cnhcirclek.org/"><b>here</b></a></h3>
+            </div>
+          </div>
+          <div className='structure_district'>
+            <h2>Major District Events</h2>
+            <ul>
+              <li><a href='https://dcon.cnhcirclek.org/'>District Convention (DCON)</a></li>
+              <li><a href='http://ftc.cnhcirclek.org/'>Fall Training Conference</a></li>
+              <li><a href='https://crazykomp.cnhcirclek.org/'>CKI South</a></li>
+              <li><a herf='http://www.cnhcirclek.org/district-large-scale-service-project-2018/'>District Large Scale Service Project (DLSSP) 2018</a></li>
+            </ul>
+          </div>
+          <div className='structure_text1'>
+            <h2>Sub-region</h2>
+            <p>Every sub-region is a collective of bordering districts based on size, demographic, and cultures. Every year at the International 
+            Convention the sub-region elects a Trustee to be the voice and the bridge to CKI international.</p>
+          </div>
+          <div className='structure_text1'>
+            <h2>International</h2>
+            <div>
+              <p>Circle K International comprises 30 districts total. At this level, there is an International President and 
+              Vice President as well as 7 elected Sub-regional Trustees and a Trustee-at-Large.</p>
+              <h3>The International Circle K's website can be found <a href="https://www.circlek.org/"><b>here</b></a></h3>
+              <h3>The International Circle K's resources can be found <a href="https://www.circlek.org/resources-main/"><b>here</b></a></h3>
+            </div>
+          </div>
         </div>
       <Footer />
     </div>
