5c83ab00dc53867d7308db6b8c98b0c65ec86573
omart980 <otorresh@ucsd.edu>
2021-07-28 09:03:38 -0700

    Updated Files
    Created Breath of the Sun and Farmers over Flowers pages

--- homestead/src/App.js
+++ homestead/src/App.js
@@ -8,6 +8,7 @@ import LParent from './login/LParent.js';
 import General from './about/subCategories/General.js';
 import OurClub from './about/subCategories/OurClub.js';
 import Structure from './about/subCategories/Structure.js';
+import Props from './Props.js';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -23,6 +24,10 @@ function App() {
       <Router>
         <Navbar />
           <Switch>
+            <Route path='/about/family/FarmersOverFlowers'><Props avatar={"https://c4.wallpaperflare.com/wallpaper/877/604/1005/anime-one-piece-minimalist-monkey-d-luffy-wallpaper-preview.jpg"} title={"Farmers over Flowers"} sidePicture={"https://cdn2.lamag.com/wp-content/uploads/sites/6/2018/03/california-poppies.jpg"} welcoming={"Welcome to Farmers over Flowers"} 
+            message={"Dreamier than any K-drama lead. The family with super cool, fun, and welcoming members. Together, we are F4!"}/></Route>
+            <Route path='/about/family/BreathoftheSun'><Props avatar={"https://wallpapercave.com/wp/wp5244984.jpg"} title={"Breath of the Sun"} sidePicture={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMmZuacljtNXB39-stJFmKB2CQhxnCveIUXQ&usqp=CAU"} welcoming={"Welcome to Breath of the Sun"}
+            message={"We are all about geeking over anime and game at full throttle! You will be placed in various challenges within these two cultures, but be rewarded as an elite, creative genius. Join us to expose your imagination. "}/></Route>
             <Route path='/about/ourclub'><OurClub /></Route>
             <Route path='/about/structure'><Structure /></Route>
             <Route path='/about'><General /></Route>
@@ -30,6 +35,7 @@ function App() {
             <Route path='/resources'><Account /></Route>
             <Route path='/calendar'><Calendar /></Route>
             <Route path='/'><Home /></Route>
+
           </Switch>
       </Router>
     </div>
