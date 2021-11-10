5542bad9af9731d530d51410d1ee33d001488421
glznn <gsm004@ucsd.edu>
2021-11-10 12:15:02 -0800

    Adjusted Calendar Categories for Mobile Version
    Added span tags to Calendar.js so that the categories are divided into separate rows. Changed values in Calendar.css. Calendar Categories are now lined up horizontally rather than vertically on mobile.

--- homestead/src/calendar/Calendar.js
+++ homestead/src/calendar/Calendar.js
@@ -8,13 +8,19 @@ function Calendar() {
   return (
     <div className="calendar">
       <div className="calendar_categories">
+        <span>
           <h1 className="calendar_categoriesService">Service</h1>
           <h1 className="calendar_categoriesCommittee">Committee</h1>
           <h1 className="calendar_categoriesSocial">Social</h1>
+        </span>
+        <span>
           <h1 className="calendar_categoriesFundraising">Fundraising</h1>
           <h1 className="calendar_categoriesKiwanis">Kiwanis</h1>
           <h1 className="calendar_categoriesDivision">Division/District</h1>
+        </span>
+        <span>
           <h1 className="calendar_categoriesLeadership">Leadership</h1>
+        </span>
         </div>
       <CalendarCogs/>
       <div className="calendar_profile">
