e1081d443de0d99b5d9b1938056162be6a0a5693
xtrasee <thn018@ucsd.edu>
2021-11-23 21:20:14 -0800

    new footer&navbar
    almost matched up footer, only need discord icon and discord link. for navbar, hover needs to be adjusted and the logo isn't centered with the title.

--- homestead/src/Footer.js
+++ homestead/src/Footer.js
@@ -1,23 +1,25 @@
 import React from "react";
 import "./Footer.css";
 import masthead from "./images/White+Masthead.png";
+import InstagramIcon from "@material-ui/icons/Instagram";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import TwitterIcon from "@material-ui/icons/Twitter";
-import InstagramIcon from "@material-ui/icons/Instagram";
 import CopyrightIcon from "@material-ui/icons/Copyright";
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footerbar">
-        <div className="footer_socialMedia">
-          <a href="https://www.facebook.com/groups/ucsdcki"><FacebookIcon /></a>
-          <a href="https://instagram.com/ucsdcirclek"><TwitterIcon /></a>
-          <a href="https://twitter.com/ucsdcircle"><InstagramIcon /></a>
+      <div className="footer">
+        <div className="footerbar">
+          <div className="footer_socialMedia">
+            <a href="https://twitter.com/ucsdcircle"><InstagramIcon /></a>
+            <a href="https://www.facebook.com/groups/ucsdcki"><FacebookIcon /></a>
+            <a href="https://instagram.com/ucsdcirclek"><TwitterIcon /></a>
+            <img src="https://spng.pinpng.com/pngs/s/57-575218_icon-free-download-png-discord-logo-png-transparent.png" width="40" height="40" ></img>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
 export default Footer;
+
--- homestead/src/Navbar.js
+++ homestead/src/Navbar.js
@@ -9,13 +9,13 @@ function Navbar() {
       <div className='navbar_image'/>
       <div className='navbar_inner'>
         <div className='navbar_upper'>
-          <Link to= '/' style={{ textDecoration: 'none' }}><div className='navbar_title'><h1>Circle K</h1><h2>at UCSD</h2></div></Link>
+          <Link to= '/' style={{ textDecoration: 'none' }}><div className='navbar_title'><img src="https://cdn-assets-cloud.frontify.com/s3/frontify-cloud-files-us/eyJwYXRoIjoiZnJvbnRpZnlcL2FjY291bnRzXC9lMFwvNzAzMTNcL3Byb2plY3RzXC85NzU1NlwvYXNzZXRzXC9iMlwvMzQzODM5OFwvZGYxMmUxNmRiMGE1MzAyZDlkM2Y5M2RmYTFhMWFhN2ItMTU2NTk4NjYyMC5wbmcifQ:frontify:AhFT1603w9DP-XpbQ6O79j4DyMkOUd7UlZ8DINgC-4k?width=2400" width="60" height="60" ></img><h2>@ UCSD</h2></div></Link>
         </div>
         <div className='navbar_lower'>
             <Link to= '/about' style={{ textDecoration: 'none' }}><NavbarOption text='About'/></Link>
             <Link to= '/calendar' style={{ textDecoration: 'none' }}><NavbarOption text='Calendar'/></Link>
             <Link to= '/resources' style={{ textDecoration: 'none' }}><NavbarOption text='Resources'/></Link>
-            <Link to= '/login' style={{ textDecoration: 'none' }}><NavbarOption text='Login'/></Link>
+            <Link to= '/login' style={{ textDecoration: 'none' }}><NavbarOption text='Log In'/></Link>
         </div>
       </div>
     </div>
